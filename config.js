b9add2a7c642b97c5f5f567efc0f905786b7fd75
Elisabeth Miller <libbymiiler09@gmail.com>
2019-02-24 12:57:29 -0800

    added cors middleware

---
+++ config.js
@@ -0,0 +1,7 @@
+//CORS config
+// {
+//   "origin": "*",
+//   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   "preflightContinue": false,
+//   "optionsSuccessStatus": 204
+// }
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
- const app = express();
+const cors = require('cors');
+const app = express();
+const { CLIENT_ORIGIN } = require('./config');
 
- const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
- app.get('/api/*', (req, res) => {
+// Express cors middleware 
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+app.get('/api/*', (req, res) => {
    res.json({ok: true});
- });
+});
 
- app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
- module.exports = {app};
+module.exports = {app};
