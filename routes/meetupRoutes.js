1d6e9745588a7a3d324ce2ce26f803e69708cb80
Elisabeth Miller <libbymiiler09@gmail.com>
2019-03-18 21:19:23 -0700

    added delete route

--- routes/meetupRoutes.js
+++ routes/meetupRoutes.js
@@ -1,17 +1,18 @@
 const _ = require('lodash');
 const mongoose = require('mongoose');
+const ObjectId = require('mongodb');
 const requireLogin = require('../middleware/requireLogin');
 
 const Meetup = mongoose.model('meetups');
 
 module.exports= (app) => {
 
-  app.get('/meetups', async (req, res) => {
+  app.get('/api/meetups', async (req, res) => {
     const meetups = await Meetup.find({});
       res.send(meetups);
   });
 
-  app.post('/meetups', async (req, res) => {
+  app.post('/api/meetups', async (req, res) => {
     const { subject, time, place } = req.body;
 
     const meetup = new Meetup({
@@ -24,10 +25,10 @@ module.exports= (app) => {
     res.send(meetup);
   });
 
-  app.delete('/meetups/:id', requireLogin, async (req, res) => {
+  app.delete('/api/meetups/:id', async (req, res) => {
     let id = req.params.id;
 
-    if (!ObjectID.isValid(id)) {
+    if (!ObjectId.isValid(id)) {
       return res.status(404).send();
     }
     Meetup.findByIdAndRemove(id).then((meetup) => {
